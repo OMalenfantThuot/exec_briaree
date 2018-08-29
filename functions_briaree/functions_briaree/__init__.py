f0ce0839942771ffc2b81ba275c75e34de29b81d
OMalenfantThuot <malenfantthuotolivier@gmail.com>
2018-08-29 11:28:15 -0400

    splitted python package from bin

--- functions_briaree/functions_briaree/__init__.py
+++
@@ -1,8 +0,0 @@
-from .createpbs import createpbs
-from .psppar_class import Psppar
-from .general import create_folder
-from .io_bigdft import *
-from .posinp_class import Posinp
-from .tab_completer import TabCompleter
-
-name = "functions_briaree"
--- functions_briaree/functions_briaree/io_bigdft/__init__.py
+++
@@ -1,1 +0,0 @@
-from .io_bigdft import read_ascii
